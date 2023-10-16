d1269bea6aa4c9167a6400a7fb21a8726da2cea4
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2023-10-16 17:09:55 +0530

    update

---
+++ src/components/Slider.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+    type: "pizzas"
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png",
+    type: "burgers"
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg",
+    type: "pizzas"
+  },
+];
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+      4000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Link href={`/menu/${data[currentSlide].type}`} className="flex z-0 flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
+      {/* TEXT CONTAINER */}
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+      </div>
+      {/* IMAGE CONTAINER */}
+      <div className="w-full flex-1 relative">
+        <Image
+          src={data[currentSlide].image}
+          alt=""
+          fill
+          className="object-cover"
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default Slider;
