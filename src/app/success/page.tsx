8f6afbae56b59dbc079b3352e063e532c9703b2f
VenkateshSircilla123 <113850972+VenkateshSircilla123@users.noreply.github.com>
2024-05-15 12:46:45 +0530

    Update page.tsx

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -13,7 +13,7 @@ const SuccessPage = () => {
     useEffect(() => {
       const makeRequest = async () => {
         try {
-          await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
+          await fetch(`https://vs-restaurant-app.vercel.app/api/confirm/${payment_intent}`, {
             method: "PUT",
           });
           setTimeout(() => {
